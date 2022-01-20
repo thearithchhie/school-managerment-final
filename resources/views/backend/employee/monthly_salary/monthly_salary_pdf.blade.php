1377affd0dcf61a5eec571737b3a1c7d48d253af
Bona-Orn <ornbona861@gmail.com>
2022-01-20 23:51:23 +0700

    Update pdf

--- resources/views/backend/employee/monthly_salary/monthly_salary_pdf.blade.php
+++ resources/views/backend/employee/monthly_salary/monthly_salary_pdf.blade.php
@@ -1,151 +1,168 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-#customers {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
+        #customers td,
+        #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
 
-#customers tr:nth-child(even){background-color: #f2f2f2;}
+        #customers tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
 
-#customers tr:hover {background-color: #ddd;}
+        #customers tr:hover {
+            background-color: #ddd;
+        }
 
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
-</style>
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4CAF50;
+            color: white;
+        }
+
+    </style>
 </head>
+
 <body>
 
 
-<table id="customers">
-  <tr>
-    <td><h2>
-  <?php $image_path = '/upload/easyschool.png'; ?>
-  <img src="{{ public_path() . $image_path }}" width="200" height="100">
+    <table id="customers">
+        <tr>
+            <td>
+                <h2>
+                    <?php $image_path = '/image/logo_3.png'; ?>
+                    <img src="{{ public_path() . $image_path }}" width="200" height="100">
 
-    </h2></td>
-    <td><h2>Easy School ERP</h2>
-<p>School Address</p>
-<p>Phone : 343434343434</p>
-<p>Email : support@easylerningbd.com</p>
-<p> <b> Employee Monthly Salary </b> </p>
+                </h2>
+            </td>
+            <td>
+                <h2>School SQ5</h2>
+                <p>No. 86A, Street 110, Russian Federation Boulevard,
+                    Sangkat Teuk Laak I, Khan Toul Kork, Phnom Penh, Cambodia</p>
+                <p>Phone : 081 67 09 09</p>
+                <p>Email : ornbona861@gmail.com</p>
+                <br>
+                <p> <b> Employee Monthly Salary </b> </p>
 
-    </td> 
-  </tr>
-  
-   
-</table>
+            </td>
+        </tr>
 
-@php 
- 
- $date = date('Y-m',strtotime($details['0']->date));
-       if ($date !='') {
-        $where[] = ['date','like',$date.'%'];
-       }
 
-$totalattend = App\Models\EmployeeAttendance::with(['user'])->where($where)->where('employee_id',$details['0']->employee_id)->get();
+    </table>
 
-        $salary = (float)$details['0']['user']['salary'];
-        $salaryperday = (float)$salary/30;
-        $absentcount = count($totalattend->where('attend_status','Absent'));
-        $totalsalaryminus = (float)$absentcount*(float)$salaryperday;
-        $totalsalary = (float)$salary-(float)$totalsalaryminus;
- 
-@endphp 
+    @php
 
-<table id="customers">
-  <tr>
-    <th width="10%">Sl</th>
-    <th width="45%">Employee Details</th>
-    <th width="45%">Employee Data</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td><b>Employee Name</b></td>
-    <td>{{ $details['0']['user']['name'] }}</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td><b>Basic Salary</b></td>
-    <td>{{ $details['0']['user']['salary'] }}</td>
-  </tr>
+        $date = date('Y-m', strtotime($details['0']->date));
+        if ($date != '') {
+            $where[] = ['date', 'like', $date . '%'];
+        }
 
-    <tr>
-    <td>3</td>
-    <td><b>Total Absent for This Month</b></td>
-    <td>{{ $absentcount }}</td>
-  </tr>
+        $totalattend = App\Models\EmployeeAttendance::with(['user'])
+            ->where($where)
+            ->where('employee_id', $details['0']->employee_id)
+            ->get();
 
-  <tr>
-    <td>4</td>
-    <td><b>Month</b></td>
-    <td>{{ date('M Y',strtotime($details['0']->date)) }}</td>
-  </tr>
-  <tr>
-    <td>5</td>
-    <td><b>Salary This Month</b></td>
-    <td>{{ $totalsalary }}</td>
-  </tr>
-    
-   
-</table>
-<br> <br>
-  <i style="font-size: 10px; float: right;">Print Data : {{ date("d M Y") }}</i>
+        $salary = (float) $details['0']['user']['salary'];
+        $salaryperday = (float) $salary / 30;
+        $absentcount = count($totalattend->where('attend_status', 'Absent'));
+        $totalsalaryminus = (float) $absentcount * (float) $salaryperday;
+        $totalsalary = (float) $salary - (float) $totalsalaryminus;
 
-<hr style="border: dashed 2px; width: 95%; color: #000000; margin-bottom: 50px">
+    @endphp
 
-<table id="customers">
-  <tr>
-    <th width="10%">Sl</th>
-    <th width="45%">Employee Details</th>
-    <th width="45%">Employee Data</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td><b>Employee Name</b></td>
-    <td>{{ $details['0']['user']['name'] }}</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td><b>Basic Salary</b></td>
-    <td>{{ $details['0']['user']['salary'] }}</td>
-  </tr>
+    <table id="customers">
+        <tr>
+            <th width="10%">Sl</th>
+            <th width="45%">Employee Details</th>
+            <th width="45%">Employee Data</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><b>Employee Name</b></td>
+            <td>{{ $details['0']['user']['name'] }}</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><b>Basic Salary</b></td>
+            <td>{{ $details['0']['user']['salary'] }}</td>
+        </tr>
 
-    <tr>
-    <td>3</td>
-    <td><b>Total Absent for This Month</b></td>
-    <td>{{ $absentcount }}</td>
-  </tr>
+        <tr>
+            <td>3</td>
+            <td><b>Total Absent for This Month</b></td>
+            <td>{{ $absentcount }}</td>
+        </tr>
 
-  <tr>
-    <td>4</td>
-    <td><b>Month</b></td>
-    <td>{{ date('M Y',strtotime($details['0']->date)) }}</td>
-  </tr>
-  <tr>
-    <td>5</td>
-    <td><b>Salary This Month</b></td>
-    <td>{{ $totalsalary }}</td>
-  </tr>
-    
-   
-</table>
-<br> <br>
-  <i style="font-size: 10px; float: right;">Print Data : {{ date("d M Y") }}</i>
+        <tr>
+            <td>4</td>
+            <td><b>Month</b></td>
+            <td>{{ date('M Y', strtotime($details['0']->date)) }}</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td><b>Salary This Month</b></td>
+            <td>{{ $totalsalary }}</td>
+        </tr>
 
- 
+
+    </table>
+    <br> <br>
+    <i style="font-size: 10px; float: right;">Print Data : {{ date('d M Y') }}</i>
+
+    <hr style="border: dashed 2px; width: 95%; color: #000000; margin-bottom: 50px">
+
+    <table id="customers">
+        <tr>
+            <th width="10%">Sl</th>
+            <th width="45%">Employee Details</th>
+            <th width="45%">Employee Data</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><b>Employee Name</b></td>
+            <td>{{ $details['0']['user']['name'] }}</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><b>Basic Salary</b></td>
+            <td>{{ $details['0']['user']['salary'] }}</td>
+        </tr>
+
+        <tr>
+            <td>3</td>
+            <td><b>Total Absent for This Month</b></td>
+            <td>{{ $absentcount }}</td>
+        </tr>
+
+        <tr>
+            <td>4</td>
+            <td><b>Month</b></td>
+            <td>{{ date('M Y', strtotime($details['0']->date)) }}</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td><b>Salary This Month</b></td>
+            <td>{{ $totalsalary }}</td>
+        </tr>
+
+
+    </table>
+    <br> <br>
+    <i style="font-size: 10px; float: right;">Print Data : {{ date('d M Y') }}</i>
+
+
 
 </body>
+
 </html>
