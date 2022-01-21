ac2ec8beafa7f71ffd528d4c1c47dee6c175eb13
Bona-Orn <ornbona861@gmail.com>
2022-01-21 18:10:51 +0700

    Religion

--- resources/views/backend/employee/employee_reg/employee_edit.blade.php
+++ resources/views/backend/employee/employee_reg/employee_edit.blade.php
@@ -7,7 +7,7 @@
  <div class="content-wrapper">
 	  <div class="container-full">
 		<!-- Content Header (Page header) -->
-	
+
 
 <section class="content">
 
@@ -15,7 +15,7 @@
 		  <div class="box">
 			<div class="box-header with-border">
 			  <h4 class="box-title">Edit Employee </h4>
-			  
+
 			</div>
 			<!-- /.box-header -->
 			<div class="box-body">
@@ -25,19 +25,19 @@
 	 <form method="post" action="{{ route('update.employee.registration',$editData->id) }}" enctype="multipart/form-data">
 	 	@csrf
 					  <div class="row">
-						<div class="col-12">	
- 
+						<div class="col-12">
 
- 	
+
+
  		<div class="row"> <!-- 1st Row -->
- 			
+
  			<div class="col-md-4">
 
  		 <div class="form-group">
 		<h5>Employee Name <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="name" class="form-control" required="" value="{{ $editData->name }}" > 
-	  </div>		 
+	 <input type="text" name="name" class="form-control" required="" value="{{ $editData->name }}" >
+	  </div>
 	  </div>
 
  			</div> <!-- End Col md 4 -->
@@ -48,10 +48,10 @@
  		 <div class="form-group">
 		<h5>Father's Name <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="fname" class="form-control" required="" value="{{ $editData->fname }}"> 
-	  </div>		 
+	 <input type="text" name="fname" class="form-control" required="" value="{{ $editData->fname }}">
 	  </div>
-	  
+	  </div>
+
  			</div> <!-- End Col md 4 -->
 
 
@@ -61,13 +61,13 @@
  		 <div class="form-group">
 		<h5>Mother's Name <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="mname" class="form-control" required="" value="{{ $editData->mname }}"> 
-	  </div>		 
+	 <input type="text" name="mname" class="form-control" required="" value="{{ $editData->mname }}">
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
- 
- 			
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
+
+
  		</div> <!-- End 1stRow -->
 
 
@@ -76,14 +76,14 @@
 
 
 	<div class="row"> <!-- 2nd Row -->
- 			
+
  			<div class="col-md-4">
 
  		 <div class="form-group">
 		<h5>Mobile Number <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="mobile" class="form-control" required="" value="{{ $editData->mobile }}" > 
-	  </div>		 
+	 <input type="text" name="mobile" class="form-control" required="" value="{{ $editData->mobile }}" >
+	  </div>
 	  </div>
 
  			</div> <!-- End Col md 4 -->
@@ -94,10 +94,10 @@
  		 <div class="form-group">
 		<h5>Address <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="address" class="form-control" required="" value="{{ $editData->address }}" > 
-	  </div>		 
+	 <input type="text" name="address" class="form-control" required="" value="{{ $editData->address }}" >
 	  </div>
-	  
+	  </div>
+
  			</div> <!-- End Col md 4 -->
 
 
@@ -111,14 +111,14 @@
 			<option value="" selected="" disabled="">Select Gender</option>
 			<option value="Male" {{ ($editData->gender == 'Male')? 'selected': '' }} >Male</option>
 			<option value="Female" {{ ($editData->gender == 'Female')? 'selected': '' }}>Female</option>
-			 
+
 		</select>
-	  </div>		 
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
- 
- 			
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
+
+
  		</div> <!-- End 2nd Row -->
 
 
@@ -134,25 +134,25 @@
 	 <select name="religion" id="religion" required="" class="form-control">
 			<option value="" selected="" disabled="">Select Religion</option>
 	<option value="Islam" {{ ($editData->religion == 'Islam')? 'selected': '' }}>Islam</option>
-	<option value="Hindu" {{ ($editData->religion == 'Hindu')? 'selected': '' }}>Hindu</option>
+	<option value="Buddhism" {{ ($editData->religion == 'Buddhism')? 'selected': '' }}>Buddhism</option>
 	<option value="Christan" {{ ($editData->religion == 'Christan')? 'selected': '' }}>Christan</option>
-			 
+
 		</select>
-	  </div>		 
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
 
 
 
- 			
+
  			<div class="col-md-4">
 
  		 <div class="form-group">
 		<h5>Date of Birth <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="date" name="dob" class="form-control" required="" value="{{ $editData->dob }}"  > 
-	  </div>		 
+	 <input type="date" name="dob" class="form-control" required="" value="{{ $editData->dob }}"  >
+	  </div>
 	  </div>
 
  			</div> <!-- End Col md 4 -->
@@ -168,14 +168,14 @@
 			 @foreach($designation as $desi)
  <option value="{{ $desi->id }}" {{ ($editData->designation_id == $desi->id)?'selected':'' }} >{{ $desi->name }}</option>
 		 	@endforeach
-			 
+
 		</select>
-	  </div>		 
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
- 
- 			
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
+
+
  		</div> <!-- End 3rd Row -->
 
 
@@ -189,11 +189,11 @@
  		 <div class="form-group">
 		<h5>Salary <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="text" name="salary" class="form-control" required="" value="{{ $editData->salary }}" > 
-	  </div>		 
+	 <input type="text" name="salary" class="form-control" required="" value="{{ $editData->salary }}" >
 	  </div>
-	  
- 			</div> <!-- End Col md 3 --> 
+	  </div>
+
+ 			</div> <!-- End Col md 3 -->
 @endif
 
 
@@ -203,11 +203,11 @@
  		<div class="form-group">
 		<h5>Joining Date <span class="text-danger">*</span></h5>
 		<div class="controls">
-	 <input type="date" name="join_date" class="form-control" required="" value="{{ $editData->join_date }}"> 
-	  </div>		 
+	 <input type="date" name="join_date" class="form-control" required="" value="{{ $editData->join_date }}">
 	  </div>
-	  
- 			</div> <!-- End Col md 3 --> 
+	  </div>
+
+ 			</div> <!-- End Col md 3 -->
 @endif
 
            <div class="col-md-3">
@@ -217,27 +217,27 @@
 		<div class="controls">
 	 <input type="file" name="image" class="form-control" id="image" >  </div>
 	 </div>
- 		 
-	  
- 			</div> <!-- End Col md 3 --> 
+
+
+ 			</div> <!-- End Col md 3 -->
 
 
  			<div class="col-md-3">
 
  		 <div class="form-group">
 		<div class="controls">
-		<img id="showImage" src="{{ (!empty($editData->image))? url('upload/employee_images/'.$editData->image):url('upload/no_image.jpg') }}" style="width: 100px; width: 100px; border: 1px solid #000000;"> 
+		<img id="showImage" src="{{ (!empty($editData->image))? url('upload/employee_images/'.$editData->image):url('upload/no_image.jpg') }}" style="width: 100px; width: 100px; border: 1px solid #000000;">
 
 	 </div>
 	 </div>
-	  
- 			</div> <!-- End Col md 3 --> 
- 
- 			
+
+ 			</div> <!-- End Col md 3 -->
+
+
  		</div> <!-- End 4TH Row -->
 
- 
-							 
+
+
 						<div class="text-xs-right">
 	 <input type="submit" class="btn btn-rounded btn-info mb-5" value="Update">
 						</div>
@@ -255,9 +255,9 @@
 		</section>
 
 
- 
- 
-	  
+
+
+
 	  </div>
   </div>
 
